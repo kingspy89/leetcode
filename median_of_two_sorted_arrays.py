f7db1464cff5116fad3a4d5b21165b18f9019fc6
kingspy89 <malav0003@gmail.com>
2025-10-30 13:47:08 +0530

    3,4

---
+++ median_of_two_sorted_arrays.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        merged = sorted(nums1 + nums2)
+        n = len(merged)
+        
+        # if odd length
+        if n % 2 == 1:
+            return float(merged[n // 2])
+        else:
+            mid1 = merged[n // 2 - 1]
+            mid2 = merged[n // 2]
+            return (mid1 + mid2) / 2.0
