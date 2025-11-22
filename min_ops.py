389128b090fc20446b971522315577894c5b5710
kingspy89 <malav0003@gmail.com>
2025-11-22 20:24:06 +0530

    Implement maxDepth function to calculate the maximum depth of a binary tree

---
+++ min_ops.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def minimumOperations(self, nums):
+        ops = 0
+        for x in nums:
+            if x % 3 != 0:
+                ops += 1
+        return ops
+        
