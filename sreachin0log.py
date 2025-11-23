53689d59859f309b6b669d6e7f09be2fc1581dbf
kingspy89 <sp640190@gmail.com>
2025-11-23 14:47:05 +0530

    Implement binary search algorithm to find target in sorted list

---
+++ sreachin0log.py
@@ -0,0 +1,12 @@
+nums = [1,3,5,6]
+target = 4
+left, right = 0, len(nums) - 1
+while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                 mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
