7a6faff51eccb313a096d04f61158e7760254d2d
kingspy89 <sp640190@gmail.com>
2025-11-27 22:16:06 +0530

    Update claiming.py

--- claiming.py
+++ claiming.py
@@ -1,12 +1,8 @@
 class Solution(object):
     def climbStairs(self, n):
-        step=n
-        for i in range(n,0,-1):
-            if i >1:
-                step=step-2
-            else:
-                step=step-1
-        return step
-                
-
-    
+        if n<=2:
+            return n
+        a,b=1,2
+        for _ in range(3,n+1):
+            a,b= b,a+b
+        return b
